9ae75baa5355215d2802af7035d8ad6b68c9939a
ebabian <babianelektra@gmail.com>
2020-05-16 13:46:44 -0400

    added more images

--- app.js
+++ app.js
@@ -12,20 +12,29 @@ $(() => {
           url: `https://api.breakingapi.com/news?q=climate&type=headlines&locale=en-US&api_key=74857994CE8A451F806D0F2A9B84FE6F`,
           data: {
             "$limit": 5,
+            "$q": "title"
       }
     }
       ).then((data) => {
-        const $update = data.articles[0].title
+        if (titleNum <= 4) {
+          data.articles[0].title
 
-         
+          const $ul = $('<ul>').attr('id', 'newsList')
 
+          const $li1 = $('<li>').html(data.articles[0].title)
+          const $li2 = $('<li>').html(data.articles[1].title)
+          const $li3 = $('<li>').html(data.articles[2].title)
 
+          $('.text-section').append($ul)
+          $ul.append($li1)
+          $ul.append($li2)
+          $ul.append($li3)
 
+          }
 
-            // console.log(data.articles[0].title);
+          console.log(data.articles[0].title);
             // console.log(data.articles[1].snippet);
             // console.log(data.articles[2].snippet);
-
 
     }).catch(err => {
       console.log(err);
