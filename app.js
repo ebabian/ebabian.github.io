7f5a57eb82d40e72ca2f2a8d51ea35aa4093439c
ebabian <babianelektra@gmail.com>
2020-05-16 18:06:06 -0400

    added event listeners to first image and a modal

--- app.js
+++ app.js
@@ -7,34 +7,58 @@ $(() => {
 
     const titleNum = 4;
 
-    $.ajax({
+    let link = `https://api.nytimes.com/svc/topstories/v2/arts.json`;
 
-          url: `https://api.breakingapi.com/news?q=climate&type=headlines&locale=en-US&api_key=74857994CE8A451F806D0F2A9B84FE6F`,
-          data: {
-            "$limit": 5,
-            "$q": "title"
-      }
+    link += '?' + $.param({
+      'api-key': "4eAfRFLc13vKAKo5Ah14UXZrjsmitkCF"
+    });
+
+    $.ajax({
+          url: link,
+          method: 'GET',
     }
       ).then((data) => {
         if (titleNum <= 4) {
-          data.articles[0].title
+          data.results[0].title
+          data.results[1].title
+          data.results[2].title
 
           const $ul = $('<ul>').attr('id', 'newsList')
 
-          const $li1 = $('<li>').html(data.articles[0].title)
-          const $li2 = $('<li>').html(data.articles[1].title)
-          const $li3 = $('<li>').html(data.articles[2].title)
+          const $li1 = $('<li>').html(data.results[0].title).attr('id', 'article1')
+          const $li2 = $('<li>').html(data.results[1].title).attr('id', 'article2')
+          const $li3 = $('<li>').html(data.results[2].title).attr('id', 'article3')
 
-          $('.text-section').append($ul)
+          $('#gloveDiv').append($ul)
           $ul.append($li1)
           $ul.append($li2)
           $ul.append($li3)
 
+          const $modal = $('#modal');
+          const $articleBtn = $('#article');
+            const modal = () => {
+
+              const $p = $('<p>').html(data.results[0].abstract)
+              $('#modal-text').append($p)
+
+              $modal.css('display', 'block');
+            $articleBtn.on('click', closeModal)
+
+        }
+            const closeModal = () => {
+            $modal.css('display', 'none')
+
+        }
+        $('#article1').on('click', modal);
           }
 
-          console.log(data.articles[0].title);
-            // console.log(data.articles[1].snippet);
-            // console.log(data.articles[2].snippet);
+          // console.log(data.results[0].title);
+          // console.log(data.results[1].title);
+          // console.log(data.results[2].title);
+
+
+
+
 
     }).catch(err => {
       console.log(err);
